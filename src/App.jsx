3016903f5e1507b9873dcea18fca9c4918b0b71d
Yash Patil <yashpatil182000@gmail.com>
2025-01-26 19:46:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Hero from "./components/Hero";
+import Num1Recipe from "./components/Num1Recipe";
+import PopularPicks from "./components/PopularPicks";
+import VegeterianPick from "./components/VegeterianPick";
+import TastyTreasuresLogo from "./assets/TastyTreasuresLogo.png";
+
 function App() {
   return (
-    <>
-      <h1 className="text-6xl font-bold underline">Vite + React</h1>
-    </>
+    <div>
+      <div className="bg-[#2B2525] py-5 flex justify-center">
+        <img src={TastyTreasuresLogo} alt="" />
+      </div>
+      <Hero />
+      <PopularPicks />
+      <VegeterianPick />
+      <Num1Recipe />
+    </div>
   );
 }
 
