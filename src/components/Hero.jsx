8d8879d02adaef6fae234bf67447f8f9a4087aa9
Yash Patil <yashpatil182000@gmail.com>
2025-01-26 19:45:36 +0530

    hero section done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div
+      className="bg-[url(./assets/hero-bg-4.jpg)] md:h-[350px] h-[250px] bg-no-repeat
+      bg-contain md:bg-cover flex items-start md:items-center
+"
+    >
+      <div className="md:w-[50%] w-[80%] md:px-10 mt-8 md:mt-0 px-5 ">
+        <p className="text-lg md:text-4xl mb-2">
+          Every Recipe You Need, <br /> One Click Away
+        </p>
+        <p className="text-xs opacity-80">
+          Browse, cook, and enjoy with our extensive recipe collection. Whether
+          you’re a novice or an expert, there’s something here for everyone!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
