e7c00bb5396c1d8deff1045a71f3e1dd94ffdf5b
Yash Patil <yashpatil182000@gmail.com>
2025-01-29 19:03:11 +0530

    hero bg img changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,15 @@ import { motion } from "framer-motion";
 function Hero() {
   return (
     <div
-      className="bg-[url(./assets/hero-bg-4.jpg)] md:h-[350px] h-[250px] bg-no-repeat
-      bg-contain md:bg-cover flex items-start md:items-center
-"
+      className="bg-[url(./assets/hero-bg-7.jpg)] md:h-[350px] h-[250px]
+      bg-cover flex items-start md:items-center"
     >
       <motion.div
         initial={{ opacity: 0, x: -50 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, ease: "easeOut" }}
         viewport={{ amount: 0.2 }}
-        className="md:w-[50%] w-[80%] md:px-10 mt-8 md:mt-0 px-5 "
+        className="md:w-[50%] w-[80%] md:px-10 mt-10 md:mt-0 px-5 "
       >
         <p className="text-lg md:text-5xl mb-2 ">
           Every Recipe You Need, <br /> One Click Away
